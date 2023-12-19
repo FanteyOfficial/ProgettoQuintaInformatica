f7dadaa471072c2912f4ee9d4a0ce900425cf2fe
FanteyOfficial <lorida1602@gmail.com>
2023-12-19 11:49:11 +0100

    query update

--- Readme/queries.sql
+++ Readme/queries.sql
@@ -10,38 +10,47 @@ CREATE TABLE Utenti (
     ultimo_accesso TIMESTAMP NOT NULL,
 
     password VARCHAR(255) NOT NULL,
-    salt VARCHAR(255) NOT NULL
+    salt VARCHAR(255) NOT NULL,
+    remember_me_token VARCHAR(255),
+
+    CONSTRAINT fk_stato FOREIGN KEY (stato) REFERENCES stati(id_stato);
 );
 
-CREATE TABLE Rubrica (
-    id_rubrica int AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Contatti (
+    id_contatto INT AUTO_INCREMENT PRIMARY KEY,
     nomeAssociato VARCHAR(255) NOT NULL,
     utente_id int,
-    FOREIGN KEY (utente_id) REFERENCES Utenti(id_utente)
+    utente_contatto_id int,
+    FOREIGN KEY (utente_id) REFERENCES Utenti(id_utente),
+    FOREIGN KEY (utente_contatto_id) REFERENCES Utenti(id_utente)
 );
 
 CREATE TABLE Chat (
-	id_chat int AUTO_INCREMENT PRIMARY KEY,
-    statoChat VARCHAR(255) NOT NULL
+	  id_chat int AUTO_INCREMENT PRIMARY KEY,
+    statoChat VARCHAR(255)
 );
 
 CREATE TABLE Messaggi (
 	id_messaggio int AUTO_INCREMENT PRIMARY KEY,
-  autore VARCHAR(255) NOT NULL,
+  utente_id VARCHAR(255),
   contenuto TEXT NOT NULL,
   ora_invio TIMESTAMP NOT NULL,
   letto TINYINT NOT NULL,
   consegnato TINYINT NOT NULL,
   chat_id int,
   FOREIGN KEY (chat_id) REFERENCES Chat(id_chat),
-  tipo TINYINT NOT NULL
+  FOREIGN KEY (utente_id) REFERENCES Utenti(id_utente)
+  tipo INT,
+  CONSTRAINT fk_tipo FOREIGN KEY (tipo) REFERENCES TipoMessaggio(id_tipo);
 );
 
 CREATE TABLE ConversaIn (
 	utente_id int,
   chat_id int,
+  contatto_id int,
   FOREIGN KEY (utente_id) REFERENCES Utenti(id_utente),
-  FOREIGN KEY (chat_id) REFERENCES Chat(id_chat)
+  FOREIGN KEY (chat_id) REFERENCES Chat(id_chat),
+  FOREIGN KEY (contatto_id) REFERENCES Contatti(id_contatto)
 );
 
 CREATE TABLE VisualizzatoDa (
@@ -52,20 +61,12 @@ CREATE TABLE VisualizzatoDa (
   FOREIGN KEY (messaggio_id) REFERENCES Messaggi(id_messaggio)
 );
 
-
-
 CREATE TABLE stati (
     id_stato TINYINT PRIMARY KEY,
     descrizione VARCHAR(255) NOT NULL
 );
 
 INSERT INTO stati (id_stato, descrizione) VALUES (1, 'Online'), (2, 'Offline');
-
-ALTER TABLE Utenti
-ADD CONSTRAINT fk_stato FOREIGN KEY (stato) REFERENCES stati(id_stato);
-
-ALTER TABLE Utenti
-ADD COLUMN remember_me_token VARCHAR(255);
 
 CREATE TABLE TipoMessaggio (
     id_tipo INT AUTO_INCREMENT PRIMARY KEY,
@@ -77,9 +78,3 @@ INSERT INTO TipoMessaggio (descrizione) VALUES
     ('Immagine'),
     ('Vocale'),
     ('File');
-
-ALTER TABLE Messaggi
-MODIFY COLUMN tipo INT;
-
-ALTER TABLE Messaggi
-ADD CONSTRAINT fk_tipo FOREIGN KEY (tipo) REFERENCES TipoMessaggio(id_tipo);
